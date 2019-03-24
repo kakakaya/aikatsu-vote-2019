d715c93aa56c9626bd6cafb81261cdb4c0f89966
kakakaya <kakakaya+git@gmail.com>
2019-03-25 01:13:09 +0900

    Implementing server

--- web.py
+++ web.py
@@ -1,13 +1,40 @@
-from flask import Flask
-app = Flask(__name__)
-from models import recent_rankings
+from flask import Flask, Response
+import json
+from models import recent_rankings, recent_rankings_for_entry
+
+app = Flask(__name__, static_url_path='', static_folder='./public')
+
+api_headers = {'Cache-Control': 'no-cache', 'Access-Control-Allow-Origin': '*'}
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return app.send_static_file('index.html')
 
 
-@app.route("/entry/<entry_name>")
-def entry(entry_name):
-    return recent_rankings()
+@app.route("/api/")
+def api_history():
+
+    for ranking in recent_rankings(hour_range=3):
+
+        print(
+            ranking.rank,
+            ranking.entry.name,
+            ranking.ranking_log.created.isoformat(timespec='seconds'),
+        )
+
+
+@app.route("/api/entry/<entry_name>")
+def api_entry(entry_name):
+    print(entry_name)
+    for ranking in recent_rankings_for_entry(entry_name, hour_range=48):
+        print(
+            ranking.rank,
+            ranking.entry.name,
+            ranking.ranking_log.created.isoformat(timespec='seconds'),
+        )
+    return Response(
+        json.dumps([]),    # TODO: impl
+        mimetype='application/json',
+        headers=api_headers,
+    )
