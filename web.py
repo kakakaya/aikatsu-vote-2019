f8f611b8efca5fe099e8f56c4d7015c2bc3c3d6b
kakakaya <kakakaya+git@gmail.com>
2019-03-28 21:46:03 +0900

    Add awful validation

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, abort, redirect
 import json
 from models import recent_rankings, recent_rankings_for_entry
 
@@ -6,17 +6,61 @@ app = Flask(__name__, static_url_path='', static_folder='./public')
 
 api_headers = {'Cache-Control': 'no-cache', 'Access-Control-Allow-Origin': '*'}
 
+# OMG, entries weren't normalized!
+entries = [
+    "星宮いちご",
+    "霧矢あおい",
+    "紫吹蘭",
+    "神崎美月",
+    "有栖川おとめ",
+    "藤堂ユリカ",
+    "一ノ瀬かえで",
+    "北大路さくら",
+    "三ノ輪ヒカリ",
+    "神谷しおん",
+    "音城セイラ",
+    "冴草きい",
+    "風沢そら",
+    "夏樹みくる",
+    "音城ノエル",
+    "大空あかり",
+    "氷上スミレ",
+    "新条ひなき",
+    "紅林珠璃",
+    "服部ユウ",
+    "天羽まどか",
+    "黒沢凛",
+    "姫里マリア",
+    "藤原みやび",
+    "栗栖ここね",
+    "堂島ニーナ",
+    "大地のの",
+    "白樺リサ",
+    "星宮りんご",
+    "光石織姫",
+    "星宮らいち",
+    "ジョニー別府",
+    "涼川直人",
+    "瀬名翼",
+    "四ツ葉春",
+]
+
 
 @app.route("/")
 def index():
     return app.send_static_file('index.html')
 
 
+@app.route("/entry/<entry_name>")
+def entry(entry_name):
+    if entry_name not in entries:
+        abort(404)
+    return Response()
+
+
 @app.route("/api/")
 def api_history():
-
     for ranking in recent_rankings(hour_range=3):
-
         print(
             ranking.rank,
             ranking.entry.name,
@@ -24,10 +68,16 @@ def api_history():
         )
 
 
-@app.route("/api/entry/<entry_name>")
+@app.route("/api/v1/entry/<entry_name>")
 def api_entry(entry_name):
     response = []
-    for ranking in recent_rankings_for_entry(entry_name, hour_range=24 * 12):
+
+    rankings = recent_rankings_for_entry(entry_name, hour_range=24 * 60)
+
+    if len(rankings) > 1000:    # Seems bad access
+        abort(404)
+
+    for ranking in rankings:
         response.append({
             'rank': ranking.rank,
             'name': ranking.entry.name,
@@ -38,3 +88,8 @@ def api_entry(entry_name):
         mimetype='application/json',
         headers=api_headers,
     )
+
+
+@app.errorhandler(404)
+def error_handler(error):
+    return redirect("/")
