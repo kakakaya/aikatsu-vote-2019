7665b7d2051c3cb0da1dac292030b1fc096e4fb7
kakakaya <kakakaya+git@gmail.com>
2019-04-01 00:17:24 +0900

    Fix ranking

--- public/scripts/recent.js
+++ public/scripts/recent.js
@@ -28,7 +28,7 @@ function render() {
             console.log(rankingLog[e]);
             rankingLog[e].forEach((entry, rank)=> {
                 rows += '<tr><td class="mdl-data-table__cell--non-numeric">' +
-                    (rank+1) + '位' + entry.slice(1) +
+                    rank + '位' + entry.slice(1) +
                     '</td></tr>';
 
             });
